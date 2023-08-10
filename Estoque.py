3729be79ba3f3dd43a861a31615a2eeeb10c16c0
Pr4ss <88332258+Pr4ss@users.noreply.github.com>
2023-08-10 16:55:38 -0300

    Update Estoque.py

--- Estoque.py
+++ Estoque.py
@@ -6,15 +6,25 @@ client_secret = "t3TezoMpOAnhJfIhXWnK7cJ2f2ryUl6o"
 redirect_uri = "https://localhost:3000/"
 access_token = "APP_USR-6879509485276765-080916-4627ac8e652cbf9a510adf19235daba5-323433124"
 
-def estoque_atualizado(ID_Produto):
+def estoque_atualizado_ML(ID_Produto):
     url = f"https://api.mercadolibre.com/items/{ID_Produto}"
     resposta = requests.get(url)
     if resposta.status_code == 200:
         dados_produto = resposta.json()
-        estoque = dados_produto.get("initial_quantity", "Não disponível")
+        estoque_ML = dados_produto.get("initial_quantity", "Não disponível")
         print(f"Estoque atualizado: {estoque}")
     else:
         print(f"Não foi possível obter o estoque do produto ID {ID_Produto}")
+
+def nome_atualizado_ML(ID_Produto):
+    url = f"https://api.mercadolibre.com/items/{ID_Produto}"
+    resposta = requests.get(url)
+    if resposta.status_code == 200:
+        dados_produto = resposta.json()
+         nome_ML = dados_produto.get("title", "Não disponível")
+        print(f"Nome atualizado: {nome_ML}")
+    else:
+        print(f"Não foi possível obter o título {ID_Produto}")
 
 def estoque_total(ID_Vendedor):
     url = f'https://api.mercadolibre.com/sites/$MLB/search?seller_id=${ID_Vendedor}'
@@ -43,10 +53,10 @@ def atualizar_estoque(ID_Produto, novo_estoque):
     if resposta.status_code == 200:
         print(f"Estoque do produto atualizado para {novo_estoque}")
     else:
-        print(f"Não foi possível atualizar o estoque do produto ID {ID_Produto}")
+        print(f"Não foi possível atualizar o estoque do produto: {ID_Produto}")
         print(resposta.status_code)
 
-def atualizar_status(ID_Produto, Status):
+def atualizar_status_ML(ID_Produto, Status):
     url = f"https://api.mercadolibre.com/items/{ID_Produto}"
     headers = {
         "Authorization": f"Bearer {access_token}",
@@ -65,17 +75,14 @@ def atualizar_status(ID_Produto, Status):
         print(resposta.status_code)
 
 
-ID_Produto = input("ID do produto: ")
+#ID_Produto = input("ID do produto: ")
 #novo_estoque = int(input("Novo estoque: "))
-novo_status = input("Novo status: ")
+#novo_status = input("Novo status: ")
 #ID_Vendedor = input("ID do vendedor: ")
 
-#estoque_total(ID_Vendedor)
+estoque_atualizado_ML(ID_Produto)
+nome_atualizado_ML(ID_Produto)
+atualizar_status_ML(ID_Produto, Status)
 
-
-#atualizar_estoque(ID_Produto, novo_estoque)
-
-#estoque_atualizado(ID_Produto)
-
-atualizar_status(ID_Produto, novo_status)
-#sexo anal
+#estoque_ML
+#nome_ML
